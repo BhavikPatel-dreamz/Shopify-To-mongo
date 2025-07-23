7048a4f942cd34b6db19749912cc9d084996ff6b
Bhavik Patel <bhavikpate2010@gmail.com>
2025-07-23 16:59:29 +0530

    Fix the server

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ startProductAddedJob();
 // Middleware
 app.use(cors());
 app.use(json());
-app.use(ipWhitelist); // Apply the IP whitelist middleware
+//app.use(ipWhitelist); // Apply the IP whitelist middleware
 
 // Routes
 app.use('/api', routes);
