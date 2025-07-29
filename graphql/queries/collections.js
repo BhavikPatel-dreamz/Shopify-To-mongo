61d6d71b250bb223f801705dee539fdb9f0d3047
Bhavik Patel <bhavikpate2010@gmail.com>
2025-07-29 10:26:39 +0530

    make colletions

--- graphql/queries/collections.js
+++ graphql/queries/collections.js
@@ -1,8 +1,38 @@
 import { gql } from "graphql-request";
 
 export const collectionsQuery = `
-  query GetCollections($cursor: String) {
+  query GetCollections($cursor: String ) {
   collections(first: 50, after: $cursor) {
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    edges {
+      node {
+        id
+        title
+        handle
+        description
+        descriptionHtml
+        updatedAt
+        image {
+          url
+          altText
+          width
+          height
+        }
+        productsCount {
+          count
+        }
+      }
+    }
+  }
+}
+`;
+
+export const collectionsQueryUpdate = `
+  query GetCollections($cursor: String , $updatedSince: String!) {
+  collections(first: 250, after: $cursor, query: $updatedSince) {
     pageInfo {
       hasNextPage
       endCursor
